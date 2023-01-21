52c0d08b44a5fc767478d06a242df610dcedef1f
Rudis <rudisjcg@gmail.com>
2023-01-21 11:29:15 -0400

    Finished

--- index.js
+++ index.js
@@ -3,26 +3,17 @@ const newsList = document.querySelector(".news__wrapper")
 async function main () {
     const news = await fetch ("https://inshorts.deta.dev/news?category=all");
     const newsData = await news.json();
-    console.log(newsData);
-
-    const random = newsData[Math.floor(Math.random() * newsData.length)];
-    console.log(random, newsData[random])
-
     newsList.innerHTML = newsData.data.slice(0,6).map((newsopt) => newsHTML(newsopt)).join("");
-
-
 }
 
 main()
-
-
 function newsHTML(newsopt) {
     return `<div class="new">
     <figure class="figure__wrapper">
         <img src=${newsopt.imageUrl} class="new__img">
         <span class="news__popper">More Details...</span>
     </figure>
-    <p class="new__para">${newsopt.content.slice(0,100)}.</p>
+    <p class="new__para">${newsopt.content.slice(0,90)}.</p>
 </div>
 `;
 }
